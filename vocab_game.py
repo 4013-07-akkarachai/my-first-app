5dab61e666fe093ce0029125f36d6c1e83585cca
4013-07-akkarachai <54251@yupparaj.ac.th>
2026-09-09 13:13:31 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -102,7 +102,7 @@ ans3 = st.text_input(
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 3: electronic device `L _ p t _ p`. 💻",
+    "ข้อ 4: electronic device `L _ p t _ p`. 💻",
     value=st.session_state.ans4_val,
 )
 # อัปเดตค่าล่าสุดเข้าตัวแปร
