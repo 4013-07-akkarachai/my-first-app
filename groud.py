2f845076cca1469ba37ee8d48cdf291732a1df07
4013-07-akkarachai <54251@yupparaj.ac.th>
2026-09-17 09:28:48 +0700

    Update groud.py

--- groud.py
+++ groud.py
@@ -135,9 +135,9 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
         st.success("เองโคตรสุดยอดเลย")
     elif 6 <= score <= 9:
         st.success("เก่งมาก")
-    elif 4 <= score <= 5:
+    elif 3 <= score <= 5:
         st.success("พอจะมีความรู้นะ")
-    elif 1 <= score <= 3:
+    elif 1 <= score <= 2:
         st.success("ได้อยู่มีความรู้นิดนึง")
     else:
         st.error("ไปอ่านหนังสือมา")
